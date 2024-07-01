fa4282a65b07f7ecefb347f15b619e4677aab83a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-01 21:55:59 +0530

    Update authController.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- server/Controllers/authController.js
+++ server/Controllers/authController.js
@@ -5,42 +5,47 @@ const User = require('../Models/User')
 
 const authController = {
     SignUp: async (req, res) => {
-        const {
-            username,
-            email, 
-            password
-        } = req.body
-
-        // console.log(req.body)
-
-        const checkUser = await User.findOne({ email: email, username: username})
-
-        if(checkUser){
-            return res.json({ Error: "User already exists...."})
-        }
-        else{
-            const passHash = await bcrypt.hash(password, 10);
-
-            if(passHash){
-                const UserNew = new User({
-                    username: username,
-                    email: email,
-                    password: passHash,
-                    Role: 'User'
-                })
-
-                const ResultUser = UserNew.save()
-
-                if(ResultUser){
-                    return res.json({ Status: "Success" })
-                }
-                else{
-                    return res.json({ Error: "Error White Inserting User to database"})
-                }
+        try{
+            const {
+                username,
+                email, 
+                password
+            } = req.body
+    
+            // console.log(req.body)
+    
+            const checkUser = await User.findOne({ email: email, username: username})
+    
+            if(checkUser){
+                return res.json({ Error: "User already exists...."})
             }
             else{
-                return  res.json({ Error: "Error while Hashing Password"})
+                const passHash = await bcrypt.hash(password, 10);
+    
+                if(passHash){
+                    const UserNew = new User({
+                        username: username,
+                        email: email,
+                        password: passHash,
+                        Role: 'User'
+                    })
+    
+                    const ResultUser = UserNew.save()
+    
+                    if(ResultUser){
+                        return res.json({ Status: "Success" })
+                    }
+                    else{
+                        return res.json({ Error: "Error White Inserting User to database"})
+                    }
+                }
+                else{
+                    return  res.json({ Error: "Error while Hashing Password"})
+                }
             }
+        }
+        catch (err) {
+            console.log(err)
         }
     },
 
@@ -52,7 +57,7 @@ const authController = {
 
         // console.log(req.body)
 
-        
+
     }
 }
 
