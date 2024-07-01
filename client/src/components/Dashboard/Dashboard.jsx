68932a9221836898486047d98a1ed82878d5871c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-01 23:03:54 +0530

    Update Dashboard.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 import  secureLocalStorage  from  "react-secure-storage"
+import { BsChatLeftFill } from "react-icons/bs";
 
 const Dashboard = () => {
     const navigate = useNavigate()
@@ -16,7 +17,10 @@ const Dashboard = () => {
     if(RoleUser !== null && EmailUser !== null){
         return (
             <div className="bg-[url('https://wallpapercave.com/wp/wp3998740.png')] bg-cover bg-center py-8 px-12 min-h-screen">
-
+                <div className="">
+                    <BsChatLeftFill className='h-16 w-auto bg-green-500 p-4 rounded-xl text-green-800 shadow-md'/>
+                    <p className="text-bold pt-1">Online ChatApp</p>
+                </div>
             </div>
         )
     }
