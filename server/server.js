655ce7888a27ffd8f9f1eb4aa953704f2740b16b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-01 16:05:19 +0530

    Update server.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const cors = require('cors');
 const path = require('path'); 
 
+const authRoute = require('./Routes/authRoute')
 
 const app = express();
 const PORT = process.env.PORT || 5000
@@ -15,6 +16,10 @@ mongoose.connect(process.env.MONGODB_URI, {
 
 app.use(cors())
 app.use(express.json())
+
+
+app.use('/auth', authRoute)
+
 
 app.listen(PORT, () => {
     console.log(`Server Running on PORT ${PORT}`)
